3ced1787d0884d48066186485783ce27a339a95d
backironanalytics <backironanalytics@gmail.com>
2024-07-17 23:06:18 -0700

    Update Luka Doncic.rksx2.js

--- src/pages/Luka Doncic.rksx2.js
+++ src/pages/Luka Doncic.rksx2.js
@@ -3,6 +3,6 @@
 // I am Testing This
 
 $w.onReady(function () {
-    $w("#html1").src = "https://raw.githubusercontent.com/backironanalytics/my-site-test/main/Luka%20Doncic.html?token=GHSAT0AAAAAACUU5CLCHSTJORJR6YPOPHV6ZUYVZOQ";
+    $w("#html1").src = "https://github.com/backironanalytics/my-site-test/blob/main/Luka%20Doncic.html";
 
 });

4821f8b23321c75e6514774fb921d5391d9dfae0
chris <backironanalytics@gmail.com>
2024-07-31 21:14:49 -0500

    Synching

--- src/pages/Players (List).hhi2y.js
+++ src/pages/Players (List).hhi2y.js
@@ -2,5 +2,9 @@
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
 
 $w.onReady(function () {
-    console.log("this is a test")
+    // Write your JavaScript here
+
+    // To select an element by ID use: $w('#elementID')
+
+    // Click 'Preview' to run your code
 });
--- src/pages/Players.vtbje.js
+++ src/pages/Players.vtbje.js
@@ -1,8 +1,10 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
-// I am Testing This
 
 $w.onReady(function () {
-    $w("#html1").src = "https://raw.githack.com/backironanalytics/my-site-test/main/Luka%20Doncic.html";
+    // Write your JavaScript here
 
+    // To select an element by ID use: $w('#elementID')
+
+    // Click 'Preview' to run your code
 });

afae52cfa7c36b36ac5ccb540808ef59390f8552
backironanalytics <backironanalytics@gmail.com>
2024-12-03 05:54:19 -0800

    12/3 commit

---
+++ Testing_parallel_test4_PLAYERREFRESH_2025_set1.R
@@ -0,0 +1,130 @@
+library(tidyverse)
+library(nbastatR)
+library(dplyr)
+library(flexdashboard)
+library(parallel)
+library(rvest)
+
+rosters <- read.csv("rosters.csv")
+rosters_id <- rosters %>% filter(Include == "Y") %>% pull(idPlayer) 
+rosters_names <- rosters %>% filter(Include == "Y") %>% pull(namePlayer)
+rosters_teams <- rosters %>% filter(Include == "Y") %>% pull(idTeam)
+
+
+
+
+Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 2)
+
+all_rosters <- seasons_rosters(seasons = 2025, return_message = FALSE)
+
+playerdata <- game_logs(seasons = 2024:2025, result_types = "player", season_types = c("Regular Season"))
+
+playerdata_playoffs <- game_logs(seasons = 2024, result_types = "player", season_types = c("Playoffs"))
+
+playerdata <- bind_rows(playerdata, playerdata_playoffs)
+
+
+player_name <- function(x){
+  
+  playerdata %>% filter(idPlayer == x) %>% 
+    select(dateGame, idGame, slugOpponent, idPlayer, namePlayer, fgm,fga,pts,treb,ast,stl,blk,fg3m,fg3a,tov,
+           plusminus,minutes,locationGame, countDaysRestPlayer, isB2B, isB2BFirst, isB2BSecond) %>% group_by(namePlayer) %>% summarize(n = n()) %>% pull(namePlayer)
+  
+}
+
+player_name <- lapply(rosters_id,player_name)
+
+players <- player_name
+
+players <- unlist(players)
+
+
+gameids<- playerdata %>% group_by(idGame) %>% summarize(n = n()) %>% pull(idGame)
+
+numcores <- parallelly::availableCores()
+
+cl <- makeCluster(numcores)
+
+my_function <- function(x) {
+  library(tidyverse)
+  library(nbastatR)
+  
+  play_play <- play_by_play_v2(game_ids = x,nest_data = F)
+  
+}
+
+results <- parLapply(cl,gameids,my_function)
+play_play <- bind_rows(results)
+
+
+stopCluster(cl)
+
+#Schedule
+
+teams <- nba_teams(league = "NBA")
+
+teams <- teams %>% filter(idLeague == 2, idConference != 0) %>% select(cityTeam, slugTeam, idTeam, nameTeam, urlThumbnailTeam) %>% rename(Opponent = cityTeam)
+
+
+slugteams <- teams %>% select(slugTeam)
+
+slugteams_list <- slugteams %>% mutate(slugTeam = tolower(slugTeam)) %>% 
+  mutate(slugTeam = ifelse(slugTeam == "uta","utah",
+                           ifelse(slugTeam == "nop","no",slugTeam))) %>% pull(slugTeam)
+
+schedule <- lapply(slugteams_list, function(x){
+  
+  testurl <- paste0("https://www.espn.com/nba/team/schedule/_/name/",x,"/seasontype/2")
+  
+  h <- read_html(testurl)
+  
+  tab <- h |> html_nodes("table")
+  
+  tab <- tab[[1]] |> html_table()
+  
+  tab <- tab |> setNames(c("Date", "Opponenet", "Time", "TV","Tickets","Tickets_dup","Unused1","Unused2")) 
+  
+  tab <- tab[-(1:2),]
+  
+  tab <- tab %>% mutate(location = ifelse(str_detect(Opponenet,"@"),"Away","Home")) %>% 
+    mutate(Opponent = ifelse(location == "Home", str_sub(Opponenet,3),str_sub(Opponenet,2))) %>% 
+    mutate(Date = str_extract(Date, '\\b[^,]+$')) %>% mutate(Date = as.Date(Date, "%b%d")) %>% select(Date,location,Opponent)
+  
+  tab <- tab %>% left_join(teams, by = "Opponent") %>% mutate(Team = toupper(x)) %>% 
+    mutate(Team = ifelse(Team == "UTAH","UTA",ifelse(Team == "NO","NOP",Team))) %>% mutate(next_game = ifelse(Date >= Sys.Date(),TRUE,FALSE)) %>% 
+    mutate(game_number = 1:n())
+  
+})
+
+schedule <- bind_rows(schedule)
+
+schedule <- schedule %>% filter(!is.na(Date))
+
+
+
+all_players <- as.data.frame(players) %>% rename(namePlayer = players) %>% 
+  left_join(playerdata %>% filter(slugSeason == "2024-25") %>% group_by(namePlayer,idPlayer,slugTeam) %>% summarize(n = n()), by = "namePlayer") %>% 
+  select(namePlayer,idPlayer,n) %>% ungroup()
+
+all_players_previous <- schedule %>% filter(Date == schedule %>% filter(next_game == TRUE) %>% pull(Date) %>% min -1) %>% pull(slugTeam)
+
+all_players_previous_batch <- lapply(all_players_previous, function(x){
+  
+  rosters %>% filter(slugTeam == x) %>% filter(Include == "Y") %>% select(idPlayer,namePlayer)
+  
+})
+
+all_players_previous_batch <- bind_rows(all_players_previous_batch)
+
+
+
+library(foreach)
+
+foreach(i = all_players_previous_batch$namePlayer, 
+        j = all_players_previous_batch$idPlayer) %do% {
+  
+  rmarkdown::render(input = 'C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/ML_Parlay_TBRv13_2025.Rmd',
+                    output_file = paste0(i,j,substr(j,start = 1,stop=3),".html"),
+                    output_dir = file.path('C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/sheets'),
+                    params = list(id = j))
+}

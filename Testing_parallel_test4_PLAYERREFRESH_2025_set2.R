ff142f058e715fb82ea7cae6a0f187996853d956
backironanalytics <backironanalytics@gmail.com>
2024-12-09 19:43:33 -0800

    12/9 commit

--- Testing_parallel_test4_PLAYERREFRESH_2025_set2.R
+++ Testing_parallel_test4_PLAYERREFRESH_2025_set2.R
@@ -125,8 +125,8 @@ all_players_previous_batch <- all_players_previous_batch %>%
 
 library(foreach)
 
-foreach(i = all_players_previous_batch$namePlayer[((length(all_players_previous_batch$namePlayer))/2+1):length(all_players_previous_batch$namePlayer)], 
-        j = all_players_previous_batch$idPlayer[((length(all_players_previous_batch$namePlayer))/2+1):length(all_players_previous_batch$namePlayer)]) %do% {
+foreach(i = all_players_previous_batch$namePlayer[((length(all_players_previous_batch$namePlayer))/2+1):length(all_players_previous_batch$namePlayer)+1], 
+        j = all_players_previous_batch$idPlayer[((length(all_players_previous_batch$namePlayer))/2+1):length(all_players_previous_batch$namePlayer)+1]) %do% {
   
   rmarkdown::render(input = 'C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/ML_Parlay_TBRv13_2025.Rmd',
                     output_file = paste0(i,j,substr(j,start = 1,stop=3),".html"),

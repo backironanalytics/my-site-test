b604b27708779de6f1587c266b6d91f89795451c
backironanalytics <backironanalytics@gmail.com>
2024-12-28 07:32:04 -0800

    matrix push new

--- Testing_parallel_test4_PLAYERREFRESH_2025_set1.R
+++ Testing_parallel_test4_PLAYERREFRESH_2025_set1.R
@@ -126,7 +126,7 @@ foreach(i = all_players_previous_batch$namePlayer[1:(length(all_players_previous
         j = all_players_previous_batch$idPlayer[1:(length(all_players_previous_batch$namePlayer)/2)]) %do% {
   
   rmarkdown::render(input = 'C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/ML_Parlay_TBRv13_2025.Rmd',
-                    output_file = paste0(i,j,substr(j,start = 1,stop=3),".html"),
+                    output_file = paste0(j,substr(j,start = 1,stop=3),".html"),
                     output_dir = file.path('C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/sheets'),
                     params = list(id = j))
 }
--- Testing_parallel_test4_PLAYERREFRESH_2025_set2.R
+++ Testing_parallel_test4_PLAYERREFRESH_2025_set2.R
@@ -126,7 +126,7 @@ foreach(i = all_players_previous_batch$namePlayer[((length(all_players_previous_
         j = all_players_previous_batch$idPlayer[((length(all_players_previous_batch$idPlayer)/2)+1):(length(all_players_previous_batch$idPlayer))]) %do% {
           
           rmarkdown::render(input = 'C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/ML_Parlay_TBRv13_2025.Rmd',
-                            output_file = paste0(i,j,substr(j,start = 1,stop=3),".html"),
+                            output_file = paste0(j,substr(j,start = 1,stop=3),".html"),
                             output_dir = file.path('C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/sheets'),
                             params = list(id = j))
         }

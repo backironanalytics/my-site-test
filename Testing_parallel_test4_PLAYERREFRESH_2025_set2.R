81304042d7d5ad422f7d7100c95d9ffe9ca55bd2
backironanalytics <backironanalytics@gmail.com>
2024-12-28 09:08:16 -0800

    12/28 ip commit

--- Testing_parallel_test4_PLAYERREFRESH_2025_set2.R
+++ Testing_parallel_test4_PLAYERREFRESH_2025_set2.R
@@ -125,7 +125,7 @@ library(foreach)
 foreach(i = all_players_previous_batch$namePlayer[((length(all_players_previous_batch$namePlayer)/2)+1):(length(all_players_previous_batch$namePlayer))], 
         j = all_players_previous_batch$idPlayer[((length(all_players_previous_batch$idPlayer)/2)+1):(length(all_players_previous_batch$idPlayer))]) %do% {
           
-          rmarkdown::render(input = 'C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/ML_Parlay_TBRv13_2025.Rmd',
+          rmarkdown::render(input = 'C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/ML_Parlay_TBRv14_2025.Rmd',
                             output_file = paste0(j,substr(j,start = 1,stop=3),".html"),
                             output_dir = file.path('C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/sheets'),
                             params = list(id = j))

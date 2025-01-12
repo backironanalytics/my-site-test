762f559e72298a3d0be2f64c03ca68b85fc6ad43
backironanalytics <backironanalytics@gmail.com>
2025-01-12 04:14:44 -0800

    1/12 commit

--- Testing_parallel_test5_PLAYERREFRESH_2025_ALL.R
+++ Testing_parallel_test5_PLAYERREFRESH_2025_ALL.R
@@ -202,6 +202,7 @@ foreach(i = all_players_previous_batch$namePlayer,
 
 })
 
+cl <- makeCluster(5)
 
 doParallel::registerDoParallel(cl)
   
@@ -237,7 +238,7 @@ foreach(j = all_players_previous_batch$idPlayer,.packages = c("flexdashboard",
           
           rmarkdown::render(input = 'C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/ML_Parlay_TBRv15_2025.Rmd',
                             output_file = paste0(j,substr(j,start = 1,stop=3),".html"),
-                            output_dir = file.path('C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/Test Files'),
+                            output_dir = file.path('C:/Users/CECRAIG/Desktop/Backironanalytics/my-site-test/sheets'),
                             params = list(id = j))
         }
 
